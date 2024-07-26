02cf215ecbc62282f47ce1d451b8bbe37679e1e3
duttaaindrila23 <166272737+duttaaindrila23@users.noreply.github.com>
2024-07-26 11:43:13 +0000

    commit 2

--- 1_Basics/conversions.js
+++ 1_Basics/conversions.js
@@ -1,4 +1,4 @@
-let score= null
+/*let score= null
 console.log(typeof score);
 let valueInNumber=Number(score)
 console.log(typeof valueInNumber);
@@ -17,3 +17,19 @@ let num=String(33)
 console.log(typeof num);
 let p="aindrila"
 console.log(p);
+***********operations********* */
+let value=3
+let negValue=-value
+console.log(negValue)
+let str1="Aindrila"
+let str2="Dutta"
+let str=str1+str2
+console.log(str);
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log(+true)
+let num1,num2,num3
+num1=num2=num3=2+2
+let gameCounter=100
+gameCounter++;
+console.log(gameCounter)
