d5dd9ca54c1b89d5f1cf01d23ab4173ae36d5674
duttaaindrila23 <166272737+duttaaindrila23@users.noreply.github.com>
2024-07-26 11:21:35 +0000

    new commit

---
+++ 1_Basics/conversions.js
@@ -0,0 +1,16 @@
+let score= null
+console.log(typeof score);
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" converts to 33
+// "33anc" converts to NAN
+//true converts to 1
+//Null converts to 0
+let isLoggedIn=1
+let bisLoggedIn=Boolean(isLoggedIn)
+console.log(bisLoggedIn);
+console.log(typeof bisLoggedIn);
+let num=String(33)
+console.log(typeof num);
