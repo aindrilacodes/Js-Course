883e0a6fd71d7c6d3adab9663b2cee1d0f0c7373
duttaaindrila23 <166272737+duttaaindrila23@users.noreply.github.com>
2024-07-26 06:59:47 +0000

    learn few topics

---
+++ 1_Basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all Js code as newer version
+//alert(3+6) we are using nodejs not browser
+console.log(3 + 3);
+console.log("Aindrila Dutta");
+let name = "Aindrila";
+let age = 21;
+let isLoggedIn = true;
+let state = null;
+//number=>2 to power of 53
+//bigint
+//String
+//boolean
+//null(It is a standalone value)
+//undefined
+//symbol->unique
+//object
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
---
+++ 1_Basics/variables.js
@@ -0,0 +1,12 @@
+const account_Id=144553
+let accountEmail="aindriladutta74@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let account_state;
+//account_Id=2 not allowed
+/* Prefer not to use var because of issue in block scope and functional scope */
+accountEmail="hcaindrila78.com"
+accountPassword="5689"
+accountCity="Kolkata"
+console.log(account_Id);
+console.table([account_Id,accountEmail,account_state,accountPassword,accountCity])
