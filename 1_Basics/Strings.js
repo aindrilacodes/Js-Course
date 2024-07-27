70ae609dc0ca11e839bc2ec8ede3bb99c3249f4c
Aindrila Dutta <166272737+aindrilacodes@users.noreply.github.com>
2024-07-27 15:53:11 +0000

    Strings commit

---
+++ 1_Basics/Strings.js
@@ -0,0 +1,21 @@
+const name="hitesh"
+const repoCount=50
+//console.log(name+" "+repoCount); alt+96(backtik shortcut)
+console.log(`hello my name is ${name} and my repocount is ${repoCount}`);
+const gameName=new String("Aindri-la")
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toLowerCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf("r"));
+console.log(gameName.substring(3));
+const str=gameName.slice(-2,8)
+console.log(str);
+const s="     aindi    "
+console.log(s.trim().length)
+console.log(s.trimEnd());
+//learn from mdn
+console.log(s.replace("n","b"));
+console.log(s.length)
+console.log(gameName.split("d"));
