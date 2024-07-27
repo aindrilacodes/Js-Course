277fbdd619304e1bba42d9fabeec767b5cbaa062
Aindrila Dutta <166272737+aindrilacodes@users.noreply.github.com>
2024-07-27 03:33:06 +0000

    another commit

---
+++ 1_Basics/comparison.js
@@ -0,0 +1,10 @@
+console.log(2>1);
+console.log(2>=7);
+ console.log("2">1);
+ console.log(null>0);
+ console.log(null>=0);
+ console.log(null==0);
+ // comparisons operator converts null to number but ==(equality) doesnt work the same way
+ console.log(undefined<0);
+ console.log(undefined>=0);
+ console.log("2"===2);//strict check where value as well as datatype are checked
