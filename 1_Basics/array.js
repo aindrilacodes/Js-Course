317a4fe3a9f10b70cd57b8843601f13d8612cea3
Aindrila Dutta <166272737+aindrilacodes@users.noreply.github.com>
2024-07-30 20:39:28 +0000

    arrays and datetime

---
+++ 1_Basics/array.js
@@ -0,0 +1,24 @@
+//Array
+ const myArray=[0,1,15,98,45]
+ const myhero=['Batman','Thor',"harley Queen"]
+ const arr=new Array(1,2,3,4)
+ myhero.join()
+
+//  console.log(arr[2]);
+//  myArray.push(96);
+
+//  console.log(myArray);
+//  myArray.unshift(9);
+//  console.log(myArray);
+//  myArray.shift();
+//  console.log(myArray);
+console.log(myArray.includes(15));
+//slice and splice
+console.log("A",myArray);
+const myn1= myArray.slice(1,3)
+console.log(myn1);
+console.log("B",myArray);
+const myn2= myArray.splice(1,3)
+//changes the original array
+console.log("C",myArray);
+console.log(myn2);
