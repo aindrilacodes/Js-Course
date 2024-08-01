ca7aa6e7f91bdab8da059ea7cf7e56d1e6f9622a
Aindrila Dutta <166272737+aindrilacodes@users.noreply.github.com>
2024-08-01 18:58:36 +0000

    functions 2

--- 1_Basics/functions.js
+++ 1_Basics/functions.js
@@ -17,10 +17,32 @@ console.log(res);
 function loginUsermsg(username="Sam") {
     //  if (username===undefined ) {
     if(!username){
-        console.log("please enter username");
+        //console.log("please enter username");
         
     } else {
         return(`${username} just logged in`)
     }
 }
-console.log(loginUsermsg());
+//console.log(loginUsermsg());
+//rest operator for multiple values
+function calculateCartPrice(val1,val2,...num1) {
+    return num1
+}
+console.log(calculateCartPrice(1,400,500,2000));
+
+const user1={
+    username:"hitesh12",
+    price: 999
+}
+
+function handleObject(anyobject) {
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`);
+    
+}
+handleObject(user1)
+const newArray=[22,45,87,9,552]
+function returnSecondValue(getArray) {
+    return getArray[2]
+    
+}
+console.log(returnSecondValue(newArray));
