f88721d82081d3b398512e478847a17d90a1b0e4
Aindrila Dutta <166272737+aindrilacodes@users.noreply.github.com>
2024-08-01 19:33:22 +0000

    scope level and mini hoisting

--- 1_Basics/functionscopes.js
+++ 1_Basics/functionscopes.js
@@ -11,3 +11,24 @@ if(true){
 console.log(a);
 //console.log(b);
 //console.log(c);
+function one() {
+    const username="ad"
+    function two() {
+        const website="you tube"
+        console.log(username);   
+    }
+    //console.log(website);
+    
+    two()
+}
+one()
+///**************interesting(hoisting)******** */
+console.log(addone(5))
+function addone(num){
+return num+1
+}
+console.log(addTwo(15))
+const addTwo=function(num){
+    return num+2
+}
+
