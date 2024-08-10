2f9ea8df00372bf6f7ab9659c825d14f8cd1685e
duttaaindrila23 <aindriladutta392@gmail.com>
2024-08-11 01:26:20 +0530

    Project 1

---
+++ Projects/script.js
@@ -0,0 +1,30 @@
+const buttons=document.querySelectorAll('.button')
+//console.log(buttons);
+const body=document.querySelector('body');
+
+buttons.forEach(function (button) {
+    console.log(button);
+    button.addEventListener('click',function(e){
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id==='grey') {
+            body.style.backgroundColor=e.target.id;
+        } 
+        if (e.target.id==='white') {
+            body.style.backgroundColor=e.target.id;
+        } 
+        if (e.target.id==='lavender') {
+            body.style.backgroundColor=e.target.id;
+        } 
+        if (e.target.id==='beige') {
+            body.style.backgroundColor=e.target.id;
+        } 
+        if (e.target.id==='pink') {
+            body.style.backgroundColor=e.target.id;
+        } 
+    })
+});
+
+    
+
+
